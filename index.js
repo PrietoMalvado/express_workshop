e9b6b1d98fa866f32f858a98c598485f4b070d6a
PrietoMalvado <chicohappy7u7@gmail.com>
2025-10-07 01:06:42 -0600

    Middleware para 404 y cambiando el body-parser

--- index.js
+++ index.js
@@ -12,10 +12,14 @@ app.use(express.urlencoded({ extended: true }));
 //:::::::::::::::::::::Inicio del servidor:::::::::::::::::::::
 
 app.get('/', (req, res, next) => {
-   return res.status(200).send('bienvenido al pokedex');// mensaje de envio del pokemon.json
+   return res.status(200).json({code: 1, message:'Bienvenido al pokedex'});// mensaje de envio del pokemon.json
 });
 
 app.use('/pokemon', pokemon);//definimos la ruta /pokemon para las rutas de pokemon
+
+app.use('/', (req, res, next ) => {
+    return res.status(404).json({code: 404, message:'URL no encontrado'});// mensaje de error si la ruta no existe
+});
 
 app.listen(process.env.PORT || 3000, () => { // Iniciamos el servidor en el puerto 3000
   console.log('Server is running on port 3000');
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -12,19 +12,19 @@ pokemon.post('/', (req, res, next) => {
 
 pokemon.get('/', async (req, res, next) => {
   const pkmn = await db.query('SELECT * FROM pokemon') //consulta a la base de datos
-  return res.status(200).json(pkmn);// envio del archivo json (GETALL POKEMON)
+  return res.status(200).json({code: 1, message: pkmn });// envio del archivo json (GETALL POKEMON)
 });
 
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
   const id = parseInt(req.params.id);//convertir entero
   const pkm = await db.query('SELECT * FROM pokemon WHERE pok_id = ?', [id]); //consulta a la base de datos
-  (id >= 1 && id <= 150 && pkm.length > 0) ? res.status(200).json(pkm[0]) : res.status(404).send("El pokemon no encontrado");// mensaje de error si el pokemon no se encuentra
+  (id >= 1 && id <= 150 && pkm.length > 0) ? res.status(200).json({code: 1, message: pkm[0] }) : res.status(404).json({code: 404, message:'Pokemon no encontrado'});// mensaje de error si el pokemon no se encuentra
 });
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => { //buscamos el pokemon por nombre
   const name = req.params.name;//obtenemos el nombre del pokemon
   const pkm = await db.query('SELECT * FROM pokemon WHERE pok_name = ?', [name]); //consulta a la base de datos
   const pkmn = pkm.filter(p => p.pok_name.toUpperCase() === name.toUpperCase()); //filtramos el pokemon por nombre sin importar mayusculas o minusculas
-  (pkmn.length > 0) ? res.status(200).json(pkmn) : res.status(404).send("Pokemon no encontrado");// mensaje de error si el pokemon no se encuentra
+  (pkmn.length > 0) ? res.status(200).json({code: 1, message: pkmn }) : res.status(404).send({code: 404, message:'Pokemon no encontrado'});// mensaje de error si el pokemon no se encuentra
 });
 
 module.exports = pokemon; //exportamos el modulo
