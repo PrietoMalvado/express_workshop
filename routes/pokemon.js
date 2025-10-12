1f76dbe21ca166a8ea5bab9b56fe30ba16ef1ca9
PrietoMalvado <chicohappy7u7@gmail.com>
2025-10-12 14:11:55 -0600

    POST en pokemon

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,8 +4,17 @@ const db = require('../config/database');
 
 //:::::::::::::::::::::METODO POST:::::::::::::::::::::
 
-pokemon.post('/', (req, res, next) => {
-   return res.status(200).send(req.body);// mensaje de envio del pokemon post
+pokemon.post('/', async (req, res, next) => {
+  const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;//desestructuramos el body
+  if( pok_name && pok_height && pok_weight && pok_base_experience){
+      let query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
+      query += ` VALUES ('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;//consulta a la base de datos
+      const rows = await db.query(query);
+      console.log(rows);
+      (rows.affectedRows == 1) ? res.status(200).json({code: 200, message:'Pokemon insertado correctamente'}) 
+      : res.status(500).json({code: 500, message:'Ocurrio un error, pokemon no insertado'});
+  }
+  return res.status(500).json({code: 500, message:'Error, campos incompletos'});
 });
 
 //:::::::::::::::::::::METODO GET:::::::::::::::::::::
@@ -18,13 +27,15 @@ pokemon.get('/', async (req, res, next) => {
 pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
   const id = parseInt(req.params.id);//convertir entero
   const pkm = await db.query('SELECT * FROM pokemon WHERE pok_id = ?', [id]); //consulta a la base de datos
-  (id >= 1 && id <= 722 && pkm.length > 0) ? res.status(200).json({code: 1, message: pkm[0] }) : res.status(404).json({code: 404, message:'Pokemon no encontrado'});// mensaje de error si el pokemon no se encuentra
+  (id >= 1 && id <= 750 && pkm.length > 0) ? res.status(200).json({code: 1, message: pkm[0] }) 
+  : res.status(404).json({code: 404, message:'Pokemon no encontrado'});// mensaje de error si el pokemon no se encuentra
 });
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => { //buscamos el pokemon por nombre
   const name = req.params.name;//obtenemos el nombre del pokemon
   const pkm = await db.query('SELECT * FROM pokemon WHERE pok_name = ?', [name]); //consulta a la base de datos
   const pkmn = pkm.filter(p => p.pok_name.toUpperCase() === name.toUpperCase()); //filtramos el pokemon por nombre sin importar mayusculas o minusculas
-  (pkmn.length > 0) ? res.status(200).json({code: 1, message: pkmn }) : res.status(404).send({code: 404, message:'Pokemon no encontrado'});// mensaje de error si el pokemon no se encuentra
+  (pkmn.length > 0) ? res.status(200).json({code: 1, message: pkmn }) 
+  : res.status(404).send({code: 404, message:'Pokemon no encontrado'});// mensaje de error si el pokemon no se encuentra
 });
 
 module.exports = pokemon; //exportamos el modulo
